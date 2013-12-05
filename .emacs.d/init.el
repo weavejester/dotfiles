dce6d23c801a26e95ebf256c392423f55fd82d39
James Reeves <jreeves@weavejester.com>
2013-12-05 22:36:40 +0000

    Removed marmalade repository from init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,6 @@
 ;; Initiate package management with Marmalade repository
 (require 'package)
 (add-to-list 'package-archives
-  '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
