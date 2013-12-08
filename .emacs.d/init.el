b2885eb6ef62ceb316a2d099cefb8d24b970bfa9
James Reeves <jreeves@weavejester.com>
2013-12-08 14:20:15 +0000

    Default to home directory

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,8 @@
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
+(setq default-directory (getenv "HOME"))
+
 (server-start)
 
 
