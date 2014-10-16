78158c9f72fa6571e412dd6c6063d08f3025860b
James Reeves <jreeves@weavejester.com>
2014-10-16 11:01:42 +0100

    Update Emacs init.el for Mac Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,10 +30,14 @@
 (setq ring-bell-function 'ignore)
 
 ;; Scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(2 ((shift) . 1))) ; one line at a time
-(setq mouse-wheel-progressive-speed nil)            ; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't)                  ; scroll window under mouse
-(setq scroll-step 1)                                ; keyboard scroll one line at a time
+;(setq mouse-wheel-scroll-amount '(2 ((shift) . 1))) ; one line at a time
+;(setq mouse-wheel-progressive-speed nil)            ; don't accelerate scrolling
+;(setq mouse-wheel-follow-mouse 't)                  ; scroll window under mouse
+;(setq scroll-step 1)                                ; keyboard scroll one line at a time
+
+;; Mac Emacs settings
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
 
 ;; Color theme
 (load-theme 'weft t)
