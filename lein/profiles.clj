844a8ade04bb023f75bb21b43c437c23e4a1d04b
James Reeves <jreeves@weavejester.com>
2014-11-11 19:05:29 +0000

    Add Leiningen user profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user
+ {:dependencies [[slamhound "1.5.5"]]
+  :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
+            [mvxcvi/whidbey "0.4.1"]
+            [lein-try "0.4.3"]]
+  :repl-options {:timeout 300000}}}
