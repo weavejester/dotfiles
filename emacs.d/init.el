5a69c5f78b078b46d8baf3dfa4ed3ee0e6407eee
James Reeves <jreeves@weavejester.com>
2014-11-14 20:52:40 +0000

    Add clojure-snippets to Emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -151,6 +151,12 @@
     (global-set-key (kbd "s-p") 'projectile-find-file)
     (evil-leader/set-key "p" 'projectile-find-file)))
 
+(use-package yasnippet
+  :init
+  (progn
+    (yas-global-mode 1)
+    (use-package clojure-snippets)))
+
 (use-package yaml-mode
   :mode ("\\.yml$" . yaml-mode))
 
