44b08c13d6ce447f070514fe6b42554cebf5714e
James Reeves <jreeves@weavejester.com>
2014-12-12 02:58:42 +0000

    Change guide-key idle to 1 second

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -154,7 +154,7 @@
   :init (guide-key-mode 1)
   :config
   (progn
-    (setq guide-key/idle-delay 0.4)
+    (setq guide-key/idle-delay 1)
     (setq guide-key/recursive-key-sequence-flag t)
     (setq guide-key/popup-window-position 'bottom)
     (setq guide-key/guide-key-sequence
