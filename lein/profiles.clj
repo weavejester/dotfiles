5975f48b4c8c44c4efe32b5064f2286223f2a484
James Reeves <jreeves@weavejester.com>
2015-04-25 17:28:57 +0100

    Update global Lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {:dependencies [[slamhound "1.5.5"]]
-  :plugins [[cider/cider-nrepl "0.8.2"]
-            [refactor-nrepl "0.1.0"]
-            [mvxcvi/whidbey "0.4.1"]
+  :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+            [refactor-nrepl "0.2.2"]
+            [lein-cljfmt "0.1.10"]
             [lein-try "0.4.3"]]
   :repl-options {:timeout 300000}}}
