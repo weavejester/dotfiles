17618709e1f36aa751f0ec52924a6a5226f56dd5
James Reeves <jreeves@weavejester.com>
2015-06-27 21:39:42 +0200

    Update Leiningen plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,6 @@
 {:user
- {:dependencies [[slamhound "1.5.5"]]
-  :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
-            [refactor-nrepl "0.2.2"]
-            [lein-cljfmt "0.1.10"]
-            [lein-try "0.4.3"]]
+ {:dependencies [[org.clojure/tools.nrepl "0.2.10" :exclusions [org.clojure/clojure]]]
+  :plugins [[cider/cider-nrepl "0.9.1"]
+            [refactor-nrepl "1.1.0-SNAPSHOT"]
+            [lein-cljfmt "0.1.10" :exclusions [org.clojure/clojure]]]
   :repl-options {:timeout 300000}}}
