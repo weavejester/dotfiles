bae48ec8e47099bcfb834e87f307b15f782cea76
James Reeves <jreeves@weavejester.com>
2015-10-31 03:02:13 +0000

    Update Lein plugin dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
  {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-            [refactor-nrepl "1.2.0-SNAPSHOT"]
-            [lein-cljfmt "0.2.1" :exclusions [org.clojure/clojure]]]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]
+            [lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]]
   :repl-options {:timeout 300000}}}
