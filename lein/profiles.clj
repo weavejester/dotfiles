9ce7cb1ce63243f6156a42c5448500af43700c5d
James Reeves <jreeves@weavejester.com>
2016-02-23 18:01:45 +0000

    Add lein-ancient to use profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugins [[lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]]}
+ {:plugins [[lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]
+            [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]]}
  :repl
  {:plugins
   [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
