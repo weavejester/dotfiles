6ca71010ef99e763eb148505a367745bcd586511
James Reeves <jreeves@weavejester.com>
2016-03-08 04:44:36 +0000

    Update lein-cljfmt plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
- {:plugins [[lein-cljfmt "0.3.0" :exclusions [org.clojure/clojure]]
+ {:plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]]}
  :repl
  {:plugins
