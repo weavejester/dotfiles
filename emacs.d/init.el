3baabb5d3cd17dc0469166b75fe16eebc589886e
James Reeves <jreeves@weavejester.com>
2017-08-06 19:00:07 +0100

    Add auto-fill-mode to markdown-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -222,7 +222,8 @@
 
 (use-package markdown-mode
   :mode (("\\.markdown$" . markdown-mode)
-         ("\\.md$" . markdown-mode)))
+         ("\\.md$" . markdown-mode))
+  :init (add-hook 'markdown-mode-hook 'auto-fill-mode))
 
 (use-package glsl-mode)
 
