a2c160321a2404ef2cc2de770a7e8de2d17e3b32
James Reeves <jreeves@weavejester.com>
2017-08-06 21:54:07 +0100

    Remove deps injected automatically by CIDER

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,7 +2,5 @@
  {:plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]]}
  :repl
- {:plugins [[cider/cider-nrepl "0.11.0"]
-            [refactor-nrepl "2.0.0"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
+ {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
   :repl-options {:timeout 300000}}}
