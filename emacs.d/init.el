ed913e3d5cc21743afc100ec85248dbcbd061403
James Reeves <jreeves@weavejester.com>
2017-08-21 20:07:19 +0100

    Add eshell-here and eshell/x functions

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -227,6 +227,27 @@
     (setq evil-cross-lines t)
     (setq evil-move-cursor-back nil)))
 
+(defun eshell-here ()
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
+(evil-leader/set-key "sh" 'eshell-here)
+
 (use-package guide-key
   :init (guide-key-mode 1)
   :config
