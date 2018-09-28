cc3db2639fe35598f4b2a31256fb24e91495a84a
James Reeves <jreeves@weavejester.com>
2018-09-28 13:34:05 +0100

    Update lein profile dependencies

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
- {:plugins [[lein-cljfmt "0.4.1" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]]}
+ {:plugins [[lein-cljfmt "0.6.1" :exclusions [org.clojure/clojure]]
+            [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]]}
  :repl
  {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
   :repl-options {:timeout 300000}}}
