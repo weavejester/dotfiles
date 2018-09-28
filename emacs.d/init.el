9b4305c231c38f9ac3a85d7a1675fcb026454dbe
James Reeves <jreeves@weavejester.com>
2018-09-28 13:59:28 +0100

    Fix eshell-here command

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -239,16 +239,14 @@
 
 (defun eshell-here ()
   (interactive)
-  (let* ((parent (if (buffer-file-name)
-                     (file-name-directory (buffer-file-name))
-                   default-directory))
+  (let* ((parent (projectile-project-root))
          (height (/ (window-total-height) 3))
          (name   (car (last (split-string parent "/" t)))))
     (split-window-vertically (- height))
     (other-window 1)
     (eshell "new")
     (rename-buffer (concat "*eshell: " name "*"))
-    (insert (concat "ls"))
+    (insert (concat "cd " parent))
     (eshell-send-input)))
 
 (evil-leader/set-key "sh" 'eshell-here)
