8234cccfcc90e11a77f2f2cd97d9d1b270d4776f
James Reeves <jreeves@weavejester.com>
2021-03-18 12:09:51 +0000

    Update Leiningen user profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
- {:plugins [[lein-cljfmt "0.6.1" :exclusions [org.clojure/clojure]]
-            [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]]}
- :repl
- {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]
-  :repl-options {:timeout 300000}}}
+ {:dependencies [[hashp "0.1.1"]]
+  ;:injections [(require 'hashp.core)]
+  :plugins [[lein-cljfmt "0.6.6" :exclusions [org.clojure/clojure]]
+            [lein-ancient "0.6.15" :exclusions [org.clojure/clojure]]
+            [lein-eftest "0.5.9"]
+            [lein-try "0.4.3"]]}}
